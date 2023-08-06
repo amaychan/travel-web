4f9a0c605fc06aa0d62159de423d30180bd7fd57
Ade Maya <adehikmayati@gmail.com>
2023-08-06 16:14:52 +0700

    navbar and cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
 import './App.css';
 import Navbar from './component/Navbar';
 import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
+import Home from './component/pages/Home';
+import Services from './component/pages/Services';
+import Products from './component/pages/Products';
+import SignUp from './component/pages/SignUp';
 function App() {
   return (
     <>
     <Router>
       <Navbar />
       <Routes>  
-        <Route path='/' exact />
+      <Route path='/' exact element= {<Home/>}/>
+      <Route path='/services' exact element= {<Services/>}/>
+        <Route path='/products' exact element= {<Products/>}/>
+        <Route path='/sign-up' exact element= {<SignUp/>}/>
       </Routes>
     </Router>
     </>
--- src/component/Button.js
+++ src/component/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.css';
 import {Link} from 'react-router-dom'
 
-const STYLES = ['btn--primary', 'btn-outline'];
+const STYLES = ['btn--primary', 'btn--outline'];
 const SIZES = ['btn--medium', 'btn--large'];
 export const Button = ({
     children, 
@@ -10,7 +10,7 @@ export const Button = ({
     onClick, 
     buttonStyle, 
     buttonSize}) =>{
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : ButtonStyle: STYLES[0];
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return (
